0d021a07f366b1850f19296d12eeec7009043bf8
whitemarketpro <91691535+whitemarketpro@users.noreply.github.com>
2023-12-24 13:30:56 +0200

    Update date-trening-ru.js

--- date-trening-ru.js
+++ date-trening-ru.js
@@ -2,7 +2,7 @@ var dateTreningElement = document.querySelector('.date-trening');
 
 if (dateTreningElement) {
   // Заменяем дату. Она должна быть в одинарных кавычках 
-  dateTreningElement.textContent = 'тест';
+  dateTreningElement.textContent = '30 января 2024';
 } else {
   // Выводим сообщение об ошибке, если элемент не найден
   console.error('Элемент с классом "date-trening" не найден.');
